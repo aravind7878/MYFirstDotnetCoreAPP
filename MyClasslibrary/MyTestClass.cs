05666c34f8236b9e34e1d11dc0ae284e9103f928
Aravind <argavini@deloitte.com>
2020-06-21 19:25:07 +0530

    Changes of full app

---
+++ MyClasslibrary/MyTestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyClasslibrary
+{
+    public static class MyTestClass
+    {
+         public static bool StartsWithUpper(this String str)
+        {
+            if (String.IsNullOrWhiteSpace(str))
+                return false;
+
+            Char ch = str[0];
+            return Char.IsUpper(ch);
+        }
+    }
+}
---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MyClasslibrary;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Test".StartsWithUpper());
+            
+
+
+        }
+    }
+}
